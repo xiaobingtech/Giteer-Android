c041e0deaf042c44b089de66740ecda55705ad95
xiaobingtech <161550081+xiaobingtech@users.noreply.github.com>
2024-08-20 13:55:44 +0800

    me_fragment

--- app/src/main/java/com/xiaobingkj/giteer/ui/me/MeFragment.kt
+++ app/src/main/java/com/xiaobingkj/giteer/ui/me/MeFragment.kt
@@ -5,56 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import cn.carbs.android.avatarimageview.library.AvatarImageView
+import com.blankj.utilcode.util.TimeUtils
+import com.blankj.utilcode.util.ToastUtils
+import com.bumptech.glide.Glide
+import com.xiaobingkj.giteer.data.storage.Storage
 import io.github.rosemoe.sora.app.R
+import io.github.rosemoe.sora.app.databinding.FragmentMeBinding
+import me.hgj.jetpackmvvm.base.fragment.BaseVmDbFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+class MeFragment : BaseVmDbFragment<MeViewModel,FragmentMeBinding>() {
+    override fun layoutId(): Int = R.layout.fragment_me
+    override fun createObserver() {
+        mViewModel.errorEvent.observe(viewLifecycleOwner) {
+            ToastUtils.showLong(it.errorLog)
+        }
+        mViewModel.userEvent.observe(viewLifecycleOwner) {
+            val avatar = mDatabind.avatar
+            if (it.avatar_url.equals("https://gitee.com/assets/no_portrait.png")) {
+                avatar.setTextAndColor(it.name.substring(0, 1), R.color.gray)
+            }else{
+                Glide.with(mActivity).load(it.avatar_url).into(avatar)
+            }
+            mDatabind.name.text = it.name
+            mDatabind.desc.text = it.bio
+            mDatabind.time.text = TimeUtils.date2String(TimeUtils.string2Date(it.created_at, "yyyy-MM-dd'T'HH:mm:ssXXX"), "yyyy-MM-dd HH:mm:ss") + "加入"
+            mDatabind.emailAddress.text = it.email
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false)
+    override fun dismissLoading() {
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun initView(savedInstanceState: Bundle?) {
+        mActivity.supportActionBar?.title = "我的"
+        mViewModel.getUser()
     }
+
+    override fun lazyLoadData() {
+
+    }
+
+    override fun showLoading(message: String) {
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mActivity.supportActionBar?.title = "我的"
+    }
+
 }
